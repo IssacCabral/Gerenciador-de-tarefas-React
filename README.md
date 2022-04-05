d216b6f88ac4fea5551284500a95a5cb996ea7a1
Clidenor Issac <issaccabrall@gmail.com>
2022-04-05 11:07:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<h2>Gerenciador de Tarefas Usando React e Typescript</h2>
+
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import Info from './components/Info'
+
+function App() {
+  return (
+    <div className="App container">
+      <Info/>
+    </div>
+  )
+}
+
+export default App
